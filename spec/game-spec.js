e01a4f6c1e8f060f93580e2ae8e0ebbfc0869cd4
dingqian <302965061@qq.com>
2016-07-27 18:23:43 +0800

    creategame class and achieve and jasmine right answer

---
+++ spec/game-spec.js
@@ -0,0 +1,24 @@
+const AnswerGenerator = require('../src/answer-generator');
+const Game = require('../src/game');
+const stdin = require('mock-stdin').stdin();
+
+describe('game',() => {
+    beforeEach(() => {
+        spyOn(AnswerGenerator, 'generate').and.returnValue('1234');
+        spyOn(console, 'log');
+        spyOn(process, 'exit');
+
+        const game = new Game();
+        game.start();
+    });
+
+    it('should congrats user when guess the right answer', () => {
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6):');
+
+        stdin.send('1234');
+
+        expect(console.log).toHaveBeenCalledWith('Congratulations!');
+        expect(process.exit).toHaveBeenCalled();
+    });
+})
