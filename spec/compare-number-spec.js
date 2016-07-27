7d34145d45e18f45c47b43e2242e3367a3e45302
preservance717 <GL717@outlook.com>
2016-07-27 17:21:42 +0800

    output is 2A2B

--- spec/compare-number-spec.js
+++ spec/compare-number-spec.js
@@ -13,6 +13,11 @@ describe('Compare number', () => {
                 answer: '1256',
                 result: '2A0B'
             },
+            {
+                input: '1234',
+                answer: '1243',
+                result: '2A2B'
+            },
         ].forEach((example) => {
             const result = CompareNumber.compare(example.input, example.answer);
             expect(result).toEqual(example.result)
