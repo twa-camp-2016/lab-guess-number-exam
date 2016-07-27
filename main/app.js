643bd7814278687248f0129305a42c5d22111e08
chenyingsho <601668970@qq.com>
2016-07-27 18:33:57 +0800

    creat play and test

--- main/app.js
+++ main/app.js
@@ -1,3 +1,18 @@
 /**
  * Created by chenying on 16-7-27.
  */
+'use strict'
+const AnswerGenerator = require('../main/modules/AnswerGenerator');
+const Play = require('../main/modules/play');
+
+console.log('Welcome!\n');
+
+const answer = AnswerGenerator.generate();
+for (let i = 6; i > 0; i--) {
+    console.log(`Please input your number(${i}):`);
+    i = Play.playGame(answer, i);
+
+}
+
+console.log(`Game Over
+${answer}`);
--- main/modules/play.js
+++ main/modules/play.js
@@ -1,10 +1,23 @@
 /**
  * Created by chenying on 16-7-27.
  */
-class Play {
-    print(answer, i) {
-    return 
+const Input = require('./input');
 
+class Play {
+    static playGame(answer, i) {
+
+        const input = Input.getInput();
+        if (Judege.judge(input)) {
+            const compareNumber = new CompareNumber();
+            const print = compareNumber.print(input, answer);
+            if(judge.judge(print)){
+                return ;
+            }
+        } else {
+            console.log('Cannot input illegal numbers!\n');
+            i++;
+        }
+        return i;
     }
 }
-module.exports=Play;
+module.exports = Play;
