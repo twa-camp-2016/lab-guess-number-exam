b90bd59ef2307311c69149db65644485a09775d5
ElsaDowney <446189226@qq.com>
2016-07-27 17:30:47 +0800

    build answerGenerator spec

--- spec/answer-generator-spec.js
+++ spec/answer-generator-spec.js
@@ -1,3 +1,16 @@
-/**
- * Created by stark on 16-7-27.
- */
+const Answer = require('../main/answer-generator');
+
+describe('Answer Generator', ()=> {
+
+    it('print answer generator', ()=> {
+        const isUnique = (item, index, array) => {
+            return array.lastIndexOf(item) === index;
+        };
+
+        const answer = Answer.generator();
+        expect(answer.length).toEqual(4);
+        expect(answer.split('').every(isUnique)).toBeTruthy();
+        expect(Answer.generator()).not.toEqual(Answer.generator());
+        
+    });
+});
