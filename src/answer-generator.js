9a8b7a964e870573fde6e258a54e0fb9d3e871c9
Zxw854953678 <Zxw854953678@163.com>
2016-07-27 17:55:01 +0800

    create and instance AnswerGenerator class

--- src/answer-generator.js
+++ src/answer-generator.js
@@ -1,13 +1,13 @@
 'use strict';
 
 class AnswerGenerator {
-  static generate(){
+  static generate() {
     const result = [];
-    const digits = [0,1,2,3,4,5,6,7,8,9];
-    for(let i=0;i<4;i++){
-      const random = parseInt(Math.random()*digits.length);
+    const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let i = 0; i < 4; i++) {
+      const random = parseInt(Math.random() * digits.length);
 
-      result.push(digits.splice(random,1)[0]);
+      result.push(digits.splice(random, 1)[0]);
     }
 
     return result.join('');
