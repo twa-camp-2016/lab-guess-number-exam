fcfd6e30fc27478037d99d925c71e2a483d7ce2a
banshengbushu <wangdanna1995@outlook.com>
2016-07-27 17:10:40 +0800

    build base files

---
+++ main/compare-number.js
@@ -0,0 +1,9 @@
+class CompareNumber{
+    static compareNumber(input,correct){
+        const inputs = input.split('');
+        const corrects = correct.split('');
+        let a = 0;
+        let b =0;
+
+    }
+}
