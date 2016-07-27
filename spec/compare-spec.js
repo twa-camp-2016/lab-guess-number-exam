a68efdb8f5e8a58e65c343799427e7510b9970fd
2smallears <2smallears@users.noreply.github.com>
2016-07-27 17:30:42 +0800

    correct mistake

--- spec/compare-spec.js
+++ spec/compare-spec.js
@@ -2,24 +2,28 @@ const CompareNumber = require('../src/models/CompareNumber');
 
 describe('CompareNumber', () => {
 
-    const test = [
-        {
-            input: '1234',
-            answer: '1234',
-            result: '4A0B'
-        },
-        {
-            input: '1234',
-            answer: '4321',
-            result: '0A4B'
-        },
-        {
-            input: '1235',
-            answer: '1234',
-            result: '3A0B'
-        }
-    ].forEach((item) => {
-        const result = CompareNumber.getCompareResult(item.input, item.answer);
-        expect(result).toEqual(item.result);
+    it('get compareResult', () => {
+
+        const test = [
+            {
+                input: '1234',
+                answer: '1234',
+                result: '4A0B'
+            },
+            {
+                input: '1234',
+                answer: '4321',
+                result: '0A4B'
+            },
+            {
+                input: '1235',
+                answer: '1234',
+                result: '3A0B'
+            }
+        ].forEach((item) => {
+            const result = CompareNumber.getCompareResult(item.input, item.answer);
+            expect(result).toEqual(item.result);
+        });
     });
+
 });
