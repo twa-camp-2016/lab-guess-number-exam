fcf4affc8cf7d9f400472b4f27b39f469d553d3b
mahong <1025383143@qq.com>
2016-07-27 17:21:45 +0800

    比较实现

--- src/compare-number.js
+++ src/compare-number.js
@@ -1,1 +1,13 @@
+class  CompareNumber{
+    static compareNumber(input,answer){
+        const inputs=input.split('');
+        const answers=answer.split('');
 
+        const digitCount=inputs.filter(c=>answers.includes(c)).length;
+        const x=inputs.filter(c=>input.indexOf(c)===answer.indexOf(c)).length;
+        const y=digitCount-x;
+        return `${x}A${y}B`
+    }
+}
+
+module.exports=CompareNumber;
