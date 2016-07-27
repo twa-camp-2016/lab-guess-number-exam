841a143b1fb0b2cc8fe8adb7d0153d4402cc3810
XLuoChen <1425145741@qq.com>
2016-07-27 17:15:23 +0800

    realize class CompareNumber

--- src/compare-number.js
+++ src/compare-number.js
@@ -1,6 +1,13 @@
-class CompareNumber{
-  compareNumber(){
+class CompareNumber {
+  static compareNumber(answer, userNumber) {
+    const answers = answer.split('');
+    const userNumbers = userNumber.split('');
 
+    const rightDigitsCount = userNumbers.filter(c => answers.some(answer => answer === c)).length;
+    const a = userNumbers.filter(c => userNumbers.indexOf(c) === answers.indexOf(c)).length;
+    const b = rightDigitsCount - a;
+
+    return `${a}A${b}B`;
   }
 }
 
