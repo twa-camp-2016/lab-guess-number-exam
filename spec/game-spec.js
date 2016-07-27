30829abf4fbf5c420ce4cd92d07abd8baf5a04e4
xiaopanglzhu <1522898103@qq.com>
2016-07-27 17:49:20 +0800

    create Game class test

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,5 +1,18 @@
+const Answer = require('../models/answer');
+const Game = require('../models/game');
+
 describe('play game', ()=> {
-  it('', ()=> {
-    expect(true).toBe(true)
+  beforeEach(()=>{
+    spyOn(Answer,'getRandomNumber').and.returnValue('1234');
+    spyOn(console,'log');
   })
+
+  it('input number repeat', ()=> {
+    spyOn(Game,'input').and.returnValue('1123');
+    Game.start();
+    expect(console.log).toHaveBeenCalledWith('Welcome!')
+    expect(console.log).toHaveBeenCalledWith('\nPlease input your number(6):')
+    expect(console.log).toHaveBeenCalledWith('Cannot input duplicate numbers!')
+  })
+
 })
