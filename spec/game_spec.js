4cfdea66f43f816ac750e7d272cc7c1d57d23767
yhl221 <1727538058@qq.com>
2016-07-27 18:02:09 +0800

    add game_spec.j

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,3 +1,48 @@
-/**
- * Created by yhl on 16-7-27.
- */
+'use strict';
+const stdin = require('mock-stdin').stdin();
+const AnswerGenerator = require('../src/answer-generator.js');
+const Game = require('../src/game.js');
+
+describe('play game', ()=> {
+    beforeEach(()=> {
+        spyOn(AnswerGenerator, 'generator').and.returnValue('1234');
+        spyOn(console, 'log');
+        spyOn(process, 'exit');
+
+        const game = new Game();
+        game.start();
+    });
+
+    it('should congrats user guess the right answer', ()=> {
+        expect(console.log).toHaveBeenCalledWith('Welcome!');
+        expect(console.log).toHaveBeenCalledWith('please input your number(6)');
+
+        stdin.send('1234');
+
+        expect(console.log).toHaveBeenCalledWith('Congratulations!');
+        expect(process.exit).toHaveBeenCalled();
+    });
+
+    it('shoule game over when tun of chances', ()=> {
+        expect(console.log).toHaveBeenCalledWith('Welcome!');
+        for (let i = 6; i > 0; i--) {
+            expect(console.log).toHaveBeenCalledWith(`please input your number(${i})`);
+            stdin.send('1235');
+            expect(console.log).toEqual('3A0B');
+        }
+
+        expect(console.log).toHaveBeenCalledWith('Game Over!');
+        expect(console.log).toHaveBeenCalledWith(`Answer:1234`);
+        expect(process.exit).toHaveBeenCalled();
+
+    });
+
+    it('should prompt invalid input', ()=> {
+        expect().toHaveBeenCalledWith('Welcome!');
+        expect().toHaveBeenCalledWith('please input your number(6)');
+
+        stdin.send('1134');
+
+        expect().toHaveBeenCalledWith('Cannot input repeat numbers!')
+    });
+});
