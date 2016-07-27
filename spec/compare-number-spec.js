3affbbf0702be7b7fb289176734ab916a2d6641c
preservance717 <GL717@outlook.com>
2016-07-27 17:21:12 +0800

    output is 2AOB

--- spec/compare-number-spec.js
+++ spec/compare-number-spec.js
@@ -7,7 +7,12 @@ describe('Compare number', () => {
                 input: '1234',
                 answer: '1234',
                 result: '4A0B'
-            }
+            },
+            {
+                input: '1234',
+                answer: '1256',
+                result: '2A0B'
+            },
         ].forEach((example) => {
             const result = CompareNumber.compare(example.input, example.answer);
             expect(result).toEqual(example.result)
