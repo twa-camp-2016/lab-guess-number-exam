5ce90836146b5879e4ea0ccb8943653e450e14cc
ElsaDowney <446189226@qq.com>
2016-07-27 18:02:38 +0800

    build game over describe

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,3 +1,42 @@
-/**
- * Created by stark on 16-7-27.
- */
+const AnswerGenerator = require('../main/answer-generator');
+const CompareNumber = require('../main/compare-number');
+const stdin = require('mock-stdin').stdin();
+const Game = require('../main/app');
+
+describe('Game',()=> {
+    beforeEach(() => {
+        spyOn(AnswerGenerator, 'generator').and.returnValue('1234');
+        spyOn(console, 'log');
+        spyOn(process, 'exit');
+
+        const game = new Game();
+        game.start();
+    });
+
+    it('should print the win information', ()=> {
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6):');
+
+        stdin.send('1234');
+
+        expect(console.log).toHaveBeenCalledWith('Congratulations!');
+        expect(process.exit).toHaveBeenCalled();
+    });
+
+    it('should print the game over',()=>{
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+
+        for (let i = 6; i > 0; i--) {
+            expect(console.log).toHaveBeenCalledWith(`Please input your number(${i}):`);
+            stdin.send('1235');
+            expect(console.log).toHaveBeenCalledWith('3A0B');
+        }
+
+        expect(console.log).toHaveBeenCalledWith('Game Over\n');
+        expect(console.log).toHaveBeenCalledWith('Answer:1234');
+        expect(process.exit).toHaveBeenCalled();
+    });
+
+});
+
+
