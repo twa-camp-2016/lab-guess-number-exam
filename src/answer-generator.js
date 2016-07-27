fc95fc8e7556621a563ce1d293a5757b9830a31a
mahong <1025383143@qq.com>
2016-07-27 17:36:36 +0800

    随机实现

--- src/answer-generator.js
+++ src/answer-generator.js
@@ -1,3 +1,13 @@
-/**
- * Created by sun on 16-7-27.
- */
+class AnswerGenerator{
+    static generate(){
+        const digits=[0,1,2,3,4,5,6,7,8,9];
+        const result=[];
+        for(let i=0;i<4;i++){
+            const randomNum=parseInt(Math.random()*digits.length);
+            result.push(digits.splice(randomNum,1)[0]);
+        }
+        return result;
+    }
+}
+
+module.exports=AnswerGenerator;
