70e6c019036289ea438d619dec5e32743e33a7e2
douqing <15829258737@163.com>
2016-07-27 17:48:37 +0800

    完成对游戏的合成及测试

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,3 +1,43 @@
-/**
- * Created by douqing on 16-7-27.
- */
+const stdin = require('mock-stdin').stdin();
+const AnswerGenerator = require('../src/answer-generator');
+const Game = require('../src/game');
+describe('game',()=>{
+    
+    beforeEach(()=>{
+        spyOn(console, 'log');
+        spyOn(AnswerGenerator, 'generate').and.returnValue('1234');
+        spyOn(process, 'exit');
+        new Game().start();
+    });
+    
+    it('should be right',()=>{
+
+        expect(console.log).toHaveBeenCalledWith('Welcome!');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6)');
+
+        stdin.send('1234');
+        expect(console.log).toHaveBeenCalledWith('Congrational!');
+        expect(process.exit).toHaveBeenCalled();
+    });
+    
+    it('should user chances 6',()=>{
+        expect(console.log).toHaveBeenCalledWith('Welcome!');
+        for (let i = 6;i>0;i--){
+            expect(console.log).toHaveBeenCalledWith(`Please input your number(${i})`);
+            stdin.send('5678');
+            expect(console.log).toHaveBeenCalledWith('0A0B');
+        }
+        expect(console.log).toHaveBeenCalledWith('Game Over!');
+        expect(console.log).toHaveBeenCalledWith('Answer:1234');
+        expect(process.exit).toHaveBeenCalled();
+        
+    });
+    
+    it('should input wrong',()=>{
+        expect(console.log).toHaveBeenCalledWith('Welcome!');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6)');
+        stdin.send('1123');
+        expect(console.log).toHaveBeenCalledWith('Can not repeat!');
+        
+    })
+});
--- src/game.js
+++ src/game.js
@@ -1,3 +1,50 @@
-/**
- * Created by douqing on 16-7-27.
- */
+const AnswerGenerator = require('./answer-generator');
+const CompareNumber = require('./compare-number');
+
+class Game{
+
+    constructor(){
+
+        this.answer = AnswerGenerator.generate();
+        this.chances = 6;
+
+        process.stdin.setEncoding('utf8');
+        process.stdin.on('data',(input)=>{
+
+            if(!this.validate(input)){
+                console.log('Can not repeat!');
+                this.ask();
+            }else {
+                const result = CompareNumber.compare(input, this.answer);
+                if (result === '4A0B'){
+                    console.log('Congrational!');
+                    process.exit();
+                }else {
+                    console.log(result);
+                    this.chances --;
+                    if (this.chances === 0){
+                        console.log('Game Over!');
+                        console.log('Answer:1234');
+                        process.exit();
+                    }
+                    this.ask();
+                }
+            }
+        });
+
+
+    }
+     validate(input){
+
+        return input.split('').every((element, index, array)=>index === array.lastIndexOf(element));
+    }
+    ask(){
+        console.log(`Please input your number(${this.chances})`);
+    }
+    start(){
+        console.log('Welcome!');
+        this.ask();
+    }
+}
+new Game().start();
+module.exports = Game;
