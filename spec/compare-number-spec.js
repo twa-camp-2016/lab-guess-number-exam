3d9e88ff5c6e89da1f78b42c3d636377255e6423
douqing <15829258737@163.com>
2016-07-27 17:20:57 +0800

    完成对数字的比对

--- spec/compare-number-spec.js
+++ spec/compare-number-spec.js
@@ -1,3 +1,15 @@
-/**
- * Created by douqing on 16-7-27.
- */
+const CompareNumber = require('../src/compare-number');
+
+describe('compare number',()=>{
+
+    it('should retuen xAXB',()=>{
+
+        [
+            {input:'7456',answer:'2345',result:'0A2B'},
+            {input:'7890',answer:'1243',result:'0A0B'}
+        ].forEach(element=>{
+            const number = CompareNumber.compare(element.input,element.answer);
+            expect(number).toEqual(element.result);
+        })
+    })
+});
--- src/compare-number.js
+++ src/compare-number.js
@@ -1,3 +1,14 @@
-/**
- * Created by douqing on 16-7-27.
- */
+class CompareNumber{
+
+    static compare(input,answer){
+        const inputArray = input.split('');
+        const answerArray = answer.split('');
+        const y = inputArray.filter(elememt=>answerArray.includes(elememt)).length;
+        const x = inputArray.filter((element,index,array)=>{
+            return array.indexOf(element) === answerArray.indexOf(element);
+        }).length;
+
+        return `${x}A${y - x}B`;
+    }
+}
+module.exports = CompareNumber;
