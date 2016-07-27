a289a79ec6e8d715672839f9af9f17abebd198e0
dingqian <302965061@qq.com>
2016-07-27 18:37:00 +0800

    jasmine same digit and game over

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -21,4 +21,29 @@ describe('game',() => {
         expect(console.log).toHaveBeenCalledWith('Congratulations!');
         expect(process.exit).toHaveBeenCalled();
     });
+
+    it('find same digit', () => {
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6):');
+ 
+        stdin.send('1224');
+
+        expect(console.log).toHaveBeenCalledWith('Cannot input duplicate numbers!');
+    });
+    
+    it('should congrats user when guess the right answer', () => {
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+        
+        for(let i = 6; i > 0; i--){
+            expect(console.log).toHaveBeenCalledWith(`Please input your number(${i}):`);
+            
+            stdin.send('5678');
+            
+            expect(console.log).toHaveBeenCalledWith('0A0B');
+
+        }
+        expect(console.log).toHaveBeenCalledWith('Game Over\n');
+        expect(console.log).toHaveBeenCalledWith('Answer:1234');
+        expect(process.exit).toHaveBeenCalled();
+    });
 })
