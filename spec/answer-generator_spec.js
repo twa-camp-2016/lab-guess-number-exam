a14be3b1054b0c01cd251e55a76458cbeb79c571
egrils <egrils@users.noreply.github.com>
2016-07-27 17:22:52 +0800

    create answer-generator

---
+++ spec/answer-generator_spec.js
@@ -0,0 +1,14 @@
+const AnswerGenerator = require('../main/answer-generator');
+
+describe('AnswerGenerator',()=>{
+    it('should generate answer',()=>{
+        const isUnique = (item,index,array)=>{
+            return array.indexOf(item)=== index;
+        };
+
+        const answer = AnswerGenerator.generate();
+        expect(answer.length).toEqual(4);
+        expect(answer.split('').every(isUnique)).toBeTruthy();
+        expect(AnswerGenerator.generate).not.toEqual(AnswerGenerator.generate());
+    });
+});
