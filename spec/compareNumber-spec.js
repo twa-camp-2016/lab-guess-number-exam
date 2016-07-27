96b0dbdf34adf74112f8323ae79c5a71bd2ebc19
gjieoo <15202979397@163.com>
2016-07-27 17:48:04 +0800

    compare number

--- spec/compareNumber-spec.js
+++ spec/compareNumber-spec.js
@@ -1,3 +1,30 @@
-/**
- * Created by guojie on 16-7-27.
- */
+const CompareNumber=require('../src/moudles/compareNumber');
+
+describe('compare number',()=> {
+    it('commpare number',()=>{
+        [{
+            input: '1234',
+            answer: '1234',
+            result: '4A0B'
+        },
+            {
+                input: '1278',
+                answer: '1234',
+                result: '2A0B'
+            },
+            {
+                input: '1243',
+                answer: '1234',
+                result: '2A2B'
+            },
+            {
+                input: '1234',
+                answer: '1789',
+                result: '1A0B'
+            }].forEach(example=> {
+            const result = CompareNumber.checkNumber(example.input, example.answer);
+            expect(result).toEqual(example.result);
+        });
+    })
+
+});
--- src/moudles/compareNumber.js
+++ src/moudles/compareNumber.js
@@ -1,3 +1,13 @@
-/**
- * Created by guojie on 16-7-27.
- */
+class  CompareNumber{
+    static checkNumber(input,answer){
+        const inputs=input.split('');
+        const answers=answer.split('');
+        
+        const rightDigitCount=inputs.filter(c=>answers.includes(c)).length;
+        const x=inputs.filter(c=>answers.indexOf(c)===inputs.indexOf(c)).length;
+        const y=rightDigitCount-x;
+        
+        return `${x}A${y}B`
+    }
+}
+module.exports=CompareNumber;
